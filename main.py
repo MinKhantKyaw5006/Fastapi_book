4ef14910d3545555ecde05f6d967ac119e21a290
MinKhantKyaw <minkhant050603@gmail.com>
2025-07-12 11:01:49 +0700

    mixing variable and query parameter

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ async def read_root():
 
 # pass parameter's variable to pass within the url to test
 
+# passing with query paramter /greet/?name=lucas
+# mix of variable nad query = greet/lucas?age=21
 
-@app.get('/greet')
-async def greet_name(name: str) -> dict:
-    return {"message": f"Hello World {name}"}
+
+@app.get('/greet/{name}')
+async def greet_name(name: str, age: int) -> dict:
+    return {"message": f"Hello World {name}", "age": age}
