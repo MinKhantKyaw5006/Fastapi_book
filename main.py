099656a95a29725acf38c472e917abb7ac6545c2
MinKhantKyaw <minkhant050603@gmail.com>
2025-07-12 21:53:41 +0700

    python crud

--- main.py
+++ main.py
@@ -1,51 +1,85 @@
-from fastapi import FastAPI, Header
-from typing import Optional
-from pydantic import BaseModel
+from fastapi import FastAPI
+
 app = FastAPI()
 
+books = [
+    {
+        "id": 1,
+        "title": "Think Python",
+        "author": "Allen B. Downey",
+        "publisher": "O'Reilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 2,
+        "title": "Django By Example",
+        "author": "Antonio Mele",
+        "publisher": "Packt Publishing Ltd",
+        "published_date": "2022-01-19",
+        "page_count": 1023,
+        "language": "English",
+    },
+    {
+        "id": 3,
+        "title": "The web socket handbook",
+        "author": "Alex Diaconu",
+        "publisher": "Xinyu Wang",
+        "published_date": "2021-01-01",
+        "page_count": 3677,
+        "language": "English",
+    },
+    {
+        "id": 4,
+        "title": "Head first Javascript",
+        "author": "Hellen Smith",
+        "publisher": "Oreilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 540,
+        "language": "English",
+    },
+    {
+        "id": 5,
+        "title": "Algorithms and Data Structures In Python",
+        "author": "Kent Lee",
+        "publisher": "Springer, Inc",
+        "published_date": "2021-01-01",
+        "page_count": 9282,
+        "language": "English",
+    },
+    {
+        "id": 6,
+        "title": "Head First HTML5 Programming",
+        "author": "Eric T Freeman",
+        "publisher": "O'Reilly Media",
+        "published_date": "2011-21-01",
+        "page_count": 3006,
+        "language": "English",
+    },
+]
 
-@app.get('/')
-async def read_root():
-    return {"message": "Hello World"}
 
-# pass parameter's variable to pass within the url to test
-
-# passing with query paramter /greet/?name=lucas
-# mix of variable nad query = greet/lucas?age=21
-# http://127.0.0.1:8000/greet?name=jona&age=21
+@app.get('/books')
+async def get_all_books():
+    return books
 
 
-@app.get('/greet')
-async def greet_name(name: Optional[str] = "User", age: int = 0) -> dict:
-    return {"message": f"Hello {name}", "age": age}
-
-# serialtion
+@app.post('/books')
+async def create_a_book():
+    pass
 
 
-class BookCreateModel(BaseModel):
-    title: str
-    author: str
+@app.get('/book/{book_id}')
+async def get_book(book_id: int) -> dict:
+    pass
 
 
-@app.post('/create_book')
-async def create_boook(book_data: BookCreateModel):
-    return {
-        "title": book_data.title,
-        "author": book_data.author
-    }
+@app.get('/book/{book_id}')
+async def update_book(book_id: int) -> dict:
+    pass
 
 
-@app.get('/get_headers', status_code=201)
-async def get_headers(
-    accept: str = Header(None),
-    content_type: str = Header(None),
-    user_agent: str = Header(None),
-    host: str = Header(None)
-):
-    request_headers = {}
-
-    request_headers["Accept"] = accept
-    request_headers["Content-Type"] = content_type
-    request_headers["User_Agent"] = user_agent
-    request_headers["Host"] = host
-    return request_headers
+@app.get('/book/{book_id}')
+async def delete_book(book_id: int) -> dict:
+    pass
